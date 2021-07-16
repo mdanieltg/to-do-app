5c14846934f2c481847f55144239b2fcb587c56d
Daniel Medrano Huerta <daniel@quicktortoise.com>
2021-07-16 18:14:52 -0500

    Agregar función reviver para traducir TaskItem.completionDate de string a Date

--- src/app/utils/storage-utils.ts
+++ src/app/utils/storage-utils.ts
@@ -1,10 +1,18 @@
 export function readFromLocalStorage<T>(name: string): T | undefined {
   const item = localStorage.getItem(name);
   return item
-         ? JSON.parse(item)
+         ? JSON.parse(item, reviver)
          : undefined;
 }
 
 export function writeToLocalStorage<T>(name: string, item: T): void {
   localStorage.setItem(name, JSON.stringify(item));
+}
+
+function reviver(key: string, value: any): any {
+  if (key === 'completionDate') {
+    value = new Date(value);
+  }
+
+  return value;
 }
